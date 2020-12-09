ffa2905179db972da79dd73d0a5036d3401c58d2
813828498@qqcom <813828498@qq.com>
2020-12-09 16:57:47 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+    <el-switch
+      v-model="value"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
     <router-view/>
   </div>
 </template>
